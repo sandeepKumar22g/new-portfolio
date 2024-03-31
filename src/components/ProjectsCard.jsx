84834d06b34ed3d00dd6f0e54131879643951b00
Sandeep Kumar <84139298+sandeepKumar22g@users.noreply.github.com>
2024-03-31 21:05:34 +0530

    Update ProjectsCard.jsx

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -7,7 +7,7 @@ const ProjectsCard = ({imgUrl, title, desc, gitURL, previewURL }) => {
     <div className='mb-5'>
         <div className='h-52 md:h-72 rounded-t-xl relative group' style={{background: `url(${imgUrl})`, backgroundSize: "cover"}}>
             <div className='overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-50 transition-all duration-500'>
-                <Link href={gitURL} className='h-14 w-14 border-2 border-[#ADB7BE] relative rounded-full hover:border-white group/link'>
+                <Link href={gitURL} target='_blank' className='h-14 w-14 border-2 border-[#ADB7BE] relative rounded-full hover:border-white group/link'>
                     <CodeBracketIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white' />
                 </Link>
                 <Link href={previewURL} target='_blank' className='h-14 w-14 border-2 m-2 border-[#ADB7BE] relative rounded-full hover:border-white group/link'>
