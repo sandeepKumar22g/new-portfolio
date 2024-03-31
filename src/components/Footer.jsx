eac7b19317e00a0477dd075a31a52f67bf6cdb16
Sandeep Kumar <84139298+sandeepKumar22g@users.noreply.github.com>
2024-03-31 22:36:47 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white ">
       <div className="container p-12 flex flex-col md:flex-row justify-between items-center mx-auto">
         <Image src="/sandeep.png" alt='logo' width={150} height={50} className='invert' />
-        <p className="text-slate-50 md:mt-0 mt-5">&copy; All rights reserved.</p>
+        <p className="text-slate-50 md:mt-0 mt-5">All rights reserved &copy; | Crafted with ❤️</p>
       </div>
     </footer>
   )
