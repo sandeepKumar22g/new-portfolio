0b3d9fcd34949f21c2193623b458f6d158f389e8
Sandeep Kumar <84139298+sandeepKumar22g@users.noreply.github.com>
2024-03-31 21:05:09 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,48 +7,32 @@ const projectData = [
         title: "E-commerce website",
         desc: "E-commerce website with the stripe payment methods to buy the items",
         imgUrl : "/ecommerce.png",
-        gitURL: "/",
+        gitURL: "https://github.com/sandeepKumar22g/e-commerce",
         previewURL: "https://ecommerce.sandeep22.xyz/"
     },
     {
         id: 2,
-        title: "E-commerce website",
-        desc: "E-commerce website with the stripe payment methods to buy the items",
-        imgUrl : "/ecommerce.png",
-        gitURL: "/",
-        previewURL: "https://ecommerce.sandeep22.xyz/"
+        title: "Crypto verse",
+        desc: "Normal crypto info realted website to see the details of the crypto currency",
+        imgUrl : "/crypto.png",
+        gitURL: "https://github.com/sandeepKumar22g/blockchain",
+        previewURL: "https://blockchaininfo.netlify.app/"
     },
     {
         id: 3,
-        title: "E-commerce website",
-        desc: "E-commerce website with the stripe payment methods to buy the items",
-        imgUrl : "/ecommerce.png",
-        gitURL: "/",
-        previewURL: "https://ecommerce.sandeep22.xyz/"
+        title: "Carhub",
+        desc: "Car hub is a website with UI/UX design with the car details",
+        imgUrl : "/carhub.png",
+        gitURL: "https://github.com/sandeepKumar22g/car_hub",
+        previewURL: "https://car-hubs.netlify.app/"
     },
     {
         id: 4,
-        title: "E-commerce website",
-        desc: "E-commerce website with the stripe payment methods to buy the items",
-        imgUrl : "/ecommerce.png",
-        gitURL: "/",
-        previewURL: "https://ecommerce.sandeep22.xyz/"
-    },
-    {
-        id: 5,
-        title: "E-commerce website",
-        desc: "E-commerce website with the stripe payment methods to buy the items",
-        imgUrl : "/ecommerce.png",
-        gitURL: "/",
-        previewURL: "https://ecommerce.sandeep22.xyz/"
-    },
-    {
-        id: 6,
-        title: "E-commerce website",
-        desc: "E-commerce website with the stripe payment methods to buy the items",
-        imgUrl : "/ecommerce.png",
-        gitURL: "/",
-        previewURL: "https://ecommerce.sandeep22.xyz/"
+        title: "Notion clone",
+        desc: "Notion clone with the functionality of realtime file edit and changes",
+        imgUrl : "/notion.png",
+        gitURL: "https://github.com/sandeepKumar22g/notion-clone",
+        previewURL: "https://notions.sandeep22.xyz/"
     },
 ]
 
